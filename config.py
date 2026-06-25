cce7ee59c3d11952ae0272563da5b21ffa18f417
ハムザ ラフィーク <38761754+RafikHamza@users.noreply.github.com>
2026-06-25 12:05:14 +0900

    Add fake secret for Gitleaks demo

---
+++ config.py
@@ -0,0 +1,4 @@
+# This file intentionally contains a FAKE secret for a cybersecurity classroom demo.
+# Do NOT use real secrets, real passwords, real tokens, or real AWS keys.
+
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
